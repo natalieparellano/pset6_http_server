20107f6526bf7d7658826210aa5d53d268d0a20b
Natalie Arellano <natalie.p.arellano@gmail.com>
2016-08-02 13:31:30 -0400

    Initial commit

---
+++ temp.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+  // char* name = "Natalie";
+  // printf("Length is %lu\n", strlen( name ));
+  char* my_var;
+
+  char* my_var = NULL; // better ...0 is also valid
+
+  // do some stuff
+
+  my_var = "sldkfjdslkjflsdkj";
+}
